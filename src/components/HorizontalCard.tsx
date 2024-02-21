3c1f14f9f996d17167127c34f40a11b00ec089c7
w4yZ <phiriyakornman@gmail.com>
2024-02-22 00:38:58 +0700

    Fix card

--- src/components/HorizontalCard.tsx
+++ src/components/HorizontalCard.tsx
@@ -19,9 +19,9 @@ export const HorizontalCard = ({ cardData }: VerticalCardProps) => {
       </div>
       <div className="p-[15px]">
         <h1 className="font-bold text-[28px]">{title}</h1>
-        <div className="flex flex-col justify-center gap-3 mb-5 mt-3 ml-10">
+        <div className="flex flex-col justify-center gap-3 mb-5 mt-3 ml-10 w-fit">
           {list.map((item, index) => (
-            <p key={index}>
+            <p key={index} className="overflow-hidden text-ellipsis whitespace-break-spaces">
               <span
                 className={`inline-block w-[5px] h-[5px] rounded-full mr-[0.5em] mb-0.5`}
                 style={{ backgroundColor: `${item.color}` }}
--- src/components/VerticalCard.tsx
+++ src/components/VerticalCard.tsx
@@ -21,7 +21,7 @@ export const VerticalCard = ({ cardData }: VerticalCardProps) => {
         <h1 className="font-bold text-[28px]">{title}</h1>
         <div className="flex flex-col justify-center gap-3 mb-5 mt-3 ml-10 w-fit">
           {list.map((item, index) => (
-            <p key={index}>
+            <p key={index} className="overflow-hidden text-ellipsis whitespace-break-spaces">
               <span
                 className={`inline-block w-[5px] h-[5px] rounded-full mr-[0.5em] mb-0.5`}
                 style={{ backgroundColor: `${item.color}` }}
