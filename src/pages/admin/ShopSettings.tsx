0f0edd67d668f78b330bd3a1fa13b230557e174e
w4yZ <phiriyakornman@gmail.com>
2024-02-24 02:37:52 +0700

    update shop setting

--- src/pages/admin/ShopSettings.tsx
+++ src/pages/admin/ShopSettings.tsx
@@ -125,108 +125,66 @@ const ShopSettings = () => {
   return (
     <main className="flex flex-row">
       <LeftSidebar />
-      <div className="flex flex-col items-center justify-center gap-4 px-6 w-[910px] my-6 mb-10 screen_910:w-full mx-auto relative">
+      <div className="flex flex-col items-center justify-center gap-4 px-6 w-[580px] my-6 mb-10 mx-auto relative">
         <div className="text-center text-5xl font-bold mt-10 mb-6 leading-[58px]">
           Update Shop Status
         </div>
-        <div className="font-bold text-center">
-          Click to change shop status{" "}
-          <span className={`${getTextColorClass(status)}`}>[{status}]</span>
-        </div>
-        <div className="flex items-center justify-center gap-4 screen_605:flex-col">
-          <button
-            className={`flex items-center justify-center gap-2 rounded-lg p-4 bg-[#4BAE4F] text-white ${
-              status === "OPEN" && "bg-[#4BAE4F]/40 cursor-not-allowed"
-            }`}
-            onClick={() => onStatusHandle("OPEN")}
-            disabled={status === "OPEN"}
-          >
-            <img src={check} alt="check" className="w-5 h-5 self-center" />
-            Set Open
-          </button>
-          <button
-            className={`flex items-center justify-center gap-2 rounded-lg p-4 bg-[#FEC006] text-white ${
-              status === "MAINTENANCE" && "bg-[#FEC006]/40 cursor-not-allowed"
-            }`}
-            onClick={() => onStatusHandle("MAINTENANCE")}
-            disabled={status === "MAINTENANCE"}
-          >
-            <img src={busy} alt="check" className="w-5 h-5 self-center" />
-            Set Maintenance
-          </button>
-          <button
-            className={`flex items-center justify-center gap-2 rounded-lg p-4 bg-[#EA3359] text-white ${
-              status === "CLOSED" && "bg-[#EA3359]/40 cursor-not-allowed"
-            }`}
-            onClick={() => onStatusHandle("CLOSED")}
-            disabled={status === "CLOSED"}
-          >
-            <img src={close} alt="check" className="w-5 h-5 self-center" />
-            Set Close
-          </button>
-        </div>
 
-        <div className="font-bold text-center">Set open - close time</div>
-        <form onSubmit={onSubmitHandle}>
-          <div className="flex gap-4 mb-4">
-            <div>
-              <label
-                htmlFor="openTime"
-                className="block mb-2 text-sm font-medium text-gray-900"
-              >
-                Open Time ({openTime})
-              </label>
-              <input
-                id="openTime"
-                type="time"
-                value={newOpenTime}
-                onChange={handleOpenTimeChange}
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                required
-              />
-            </div>
-            <div>
-              <label
-                htmlFor="closeTime"
-                className="block mb-2 text-sm font-medium text-gray-900"
-              >
-                Close Time ({closeTime})
-              </label>
-              <input
-                id="closeTime"
-                type="time"
-                value={newCloseTime}
-                onChange={handleCloseTimeChange}
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                required
-              />
-            </div>
+        <div className="w-full rounded-xl border shadow-md">
+          <div className="flex flex-col space-y-1.5 p-6 font-bold text-center text-xl">
+            Click to change shop status{" "}
+            <span className={`${getTextColorClass(status)}`}>[{status}]</span>
           </div>
-          <button
-            type="submit"
-            className={`bg-[#1EAEF0] rounded-[10px] px-12 py-2 opacity-100 hover:opacity-80 font-bold text-white w-full`}
-          >
-            อัพเดตเวลา
-          </button>
-        </form>
-
-        {/* SET RATE PRICE */}
-        <div className="text-center text-5xl font-bold mt-10 mb-3 leading-[58px]">
-          SET RATE PRICE
+          <div className="flex items-center justify-center gap-4 p-6 pt-0 screen_605:flex-col">
+            <button
+              className={`flex items-center justify-center gap-2 rounded-lg p-4 bg-[#4BAE4F] text-white ${
+                status === "OPEN" && "bg-[#4BAE4F]/40 cursor-not-allowed"
+              }`}
+              onClick={() => onStatusHandle("OPEN")}
+              disabled={status === "OPEN"}
+            >
+              <img src={check} alt="check" className="w-5 h-5 self-center" />
+              Set Open
+            </button>
+            <button
+              className={`flex items-center justify-center gap-2 rounded-lg p-4 bg-[#FEC006] text-white ${
+                status === "MAINTENANCE" && "bg-[#FEC006]/40 cursor-not-allowed"
+              }`}
+              onClick={() => onStatusHandle("MAINTENANCE")}
+              disabled={status === "MAINTENANCE"}
+            >
+              <img src={busy} alt="check" className="w-5 h-5 self-center" />
+              Set Maintenance
+            </button>
+            <button
+              className={`flex items-center justify-center gap-2 rounded-lg p-4 bg-[#EA3359] text-white ${
+                status === "CLOSED" && "bg-[#EA3359]/40 cursor-not-allowed"
+              }`}
+              onClick={() => onStatusHandle("CLOSED")}
+              disabled={status === "CLOSED"}
+            >
+              <img src={close} alt="check" className="w-5 h-5 self-center" />
+              Set Close
+            </button>
+          </div>
         </div>
-        <div className="bg-emerald-500/20 p-3 rounded-md flex items-center gap-x-2 text-sm text-emerald-500">
-          <FaBahtSign
-            className="rounded-full bg-emerald-500 text-white/80 p-1"
-            size={20}
-          />
-          <p>
-            Current rate is{" "}
-            <span className="font-bold text-lg">"{initialRate}"</span>
-          </p>
-        </div>
 
-        <form onSubmit={onUpdateRate}>
-          <div className="flex items-center gap-x-2 mb-4">
+        {/* Edit content */}
+        <div className="w-full rounded-xl border shadow-md">
+          <div className="flex flex-col space-y-1.5 p-6 font-bold text-center text-xl">
+            Edit content (WIP)
+          </div>
+          <div className="p-6 pt-0 flex flex-col gap-4">
+            <p className="text-center">
+              Current content text is{" "}
+              <span className="font-bold text-lg">
+                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Et,
+                saepe!"
+              </span>
+            </p>
+
+            <form>
+              {/* <div className="flex items-center gap-x-2 mb-4">
             <input
               type="number"
               name="rate"
@@ -239,20 +197,117 @@ const ShopSettings = () => {
             <div className="">
               บาท : <span className="font-bold">100</span> v-bucks
             </div>
+          </div> */}
+              <button
+                type="submit"
+                className={`bg-[#1EAEF0] rounded-[10px] px-12 py-2 opacity-100 hover:opacity-80 font-bold text-white w-full cursor-not-allowed disabled:bg-[#1EAEF0]/50 disabled:hover:opacity-100`}
+                disabled={true}
+              >
+                อัพเดต Content
+              </button>
+            </form>
           </div>
-          <button
-            type="submit"
-            className={`bg-[#1EAEF0] rounded-[10px] px-12 py-2 opacity-100 hover:opacity-80 font-bold text-white w-full`}
+        </div>
+
+        <div className="w-full rounded-xl border shadow-md">
+          <div className="flex flex-col space-y-1.5 p-6 font-bold text-center text-xl mt-4">
+            Set open - close time
+          </div>
+          <form
+            onSubmit={onSubmitHandle}
+            className="p-6 pt-0 flex flex-col gap-4"
           >
-            อัพเดตเรทราคา
-          </button>
-        </form>
-        <Link
-          to={"/item-shop"}
-          className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 text-primary underline-offset-4 hover:underline mt-2"
-        >
-          Go to item shop
-        </Link>
+            <div className="flex items-center justify-center gap-4 mb-4">
+              <div>
+                <label
+                  htmlFor="openTime"
+                  className="block mb-2 text-sm font-medium text-gray-900"
+                >
+                  Open Time ({openTime})
+                </label>
+                <input
+                  id="openTime"
+                  type="time"
+                  value={newOpenTime}
+                  onChange={handleOpenTimeChange}
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="closeTime"
+                  className="block mb-2 text-sm font-medium text-gray-900"
+                >
+                  Close Time ({closeTime})
+                </label>
+                <input
+                  id="closeTime"
+                  type="time"
+                  value={newCloseTime}
+                  onChange={handleCloseTimeChange}
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                  required
+                />
+              </div>
+            </div>
+            <button
+              type="submit"
+              className={`bg-[#1EAEF0] rounded-[10px] px-12 py-2 opacity-100 hover:opacity-80 font-bold text-white w-full`}
+            >
+              อัพเดตเวลา
+            </button>
+          </form>
+        </div>
+        {/* SET RATE PRICE */}
+        <div className="text-center text-5xl font-bold mt-10 mb-3 leading-[58px]">
+          SET RATE PRICE
+        </div>
+        <div className="w-full rounded-xl border shadow-md">
+          <div className="p-6 pt-6 flex flex-col gap-4">
+            <div className="flex flex-col items-center justify-center">
+              <div className="bg-emerald-500/20 p-3 w-fit rounded-md flex items-center gap-x-2 text-sm text-emerald-500">
+                <FaBahtSign
+                  className="rounded-full bg-emerald-500 text-white/80 p-1"
+                  size={20}
+                />
+                <p>
+                  Current rate is{" "}
+                  <span className="font-bold text-lg">"{initialRate}"</span>
+                </p>
+              </div>
+            </div>
+
+            <form onSubmit={onUpdateRate} className="flex flex-col items-center justify-center">
+              <div className="flex items-center gap-x-2 mb-4">
+                <input
+                  type="number"
+                  name="rate"
+                  value={updateRate}
+                  onChange={handleRateChange}
+                  min={0}
+                  max={100}
+                  className="w-[60px] border border-blue-gray-50 rounded-[5px] px-[10px] py-[5px] focus:border-[#1EAEF0] outline-[#02A7F3]"
+                />
+                <div className="">
+                  บาท : <span className="font-bold">100</span> v-bucks
+                </div>
+              </div>
+              <button
+                type="submit"
+                className={`bg-[#1EAEF0] rounded-[10px] px-12 py-2 opacity-100 hover:opacity-80 font-bold text-white w-full`}
+              >
+                อัพเดตเรทราคา
+              </button>
+            </form>
+            <Link
+              to={"/item-shop"}
+              className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 text-primary underline-offset-4 hover:underline mt-2"
+            >
+              Go to item shop
+            </Link>
+          </div>
+        </div>
       </div>
     </main>
   );
