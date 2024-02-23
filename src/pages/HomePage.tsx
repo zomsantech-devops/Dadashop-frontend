ed640bcd48126ae0fcd3d0edb32ea625b06f99dd
thiwakonsakunchao <thiwakon_sakunchao@cmu.ac.th>
2024-02-23 19:05:52 +0700

    Delete font bold other service in navbar

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,6 +5,7 @@ import axios from "axios";
 import check from "../assets/icons/check.svg";
 import busy from "../assets/icons/busy.svg";
 import close from "../assets/icons/close.svg";
+import Footer from "../components/shared/Footer";
 
 function HomePage() {
   const [status, setStatus] = useState<string>("");
@@ -70,7 +71,7 @@ function HomePage() {
       )}
       <div className="flex screen_1250:flex-col self-center gap-8 screen_960:gap-10 screen_500:w-full">
         <div className="flex w-[600px] h-[475px] rounded-3xl bg-lime-100 overflow-hidden bg-gradient-to-b from-[#B0DE90] via-[#4AA155] via-80% to-[#3A9A4E] price-and-how-to-box screen_610:w-[450px] screen_610:self-center screen_500:w-[350px] screen_500:h-[350px]">
-          <div className="relative flex-1  justify-center items-center">
+          <div className="relative flex-1 flex justify-center items-center">
             <img
               src={wraith}
               alt="vBucksImage"
@@ -101,7 +102,7 @@ function HomePage() {
         </div>
 
         <div className="relative flex w-[600px] h-[475px] rounded-3xl bg-lime-100 overflow-hidden bg-gradient-to-b from-[#80E2FF] via-[#7ed0fc] via-80% to-[#56ADF3] price-and-how-to-box screen_610:w-[450px] screen_610:self-center screen_500:w-[350px] screen_500:h-[350px]">
-          <div className="flex-1 justify-center items-center">
+          <div className="flex-1 flex justify-center items-center">
             <img
               src={wraith}
               alt="vBucksImage"
@@ -111,7 +112,7 @@ function HomePage() {
           <div className="flex-1 flex justify-start items-center z-20">
             <div className="w-full">
               <div className="text-3xl font-bold text-white mb-2 screen_500:text-2xl screen_500:text-center">
-                เติมเกมอื่นๆ
+                บริการอื่นๆ
               </div>
               <div className="flex gap-1.5 screen_500:justify-center">
                 <Link
@@ -125,7 +126,9 @@ function HomePage() {
           </div>
         </div>
       </div>
+      <Footer />
     </div>
+   
   );
 }
 
